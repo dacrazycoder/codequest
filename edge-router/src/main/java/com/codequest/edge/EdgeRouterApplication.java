1ab23ceed7d692dada93c1486f2fbb0d13b57d24
Shane.Pettit <shane.pettit@sita.aero>
2018-03-07 21:16:24 -0500

    Adding edge-router config

--- edge-router/src/main/java/com/codequest/edge/EdgeRouterApplication.java
+++ edge-router/src/main/java/com/codequest/edge/EdgeRouterApplication.java
@@ -2,8 +2,12 @@ package com.codequest.edge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulServer
 public class EdgeRouterApplication {
 
 	public static void main(String[] args) {
