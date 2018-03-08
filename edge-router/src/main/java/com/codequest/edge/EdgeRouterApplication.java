8d9dba6f671fa7ae3dcef4c65d5067021d21d3c6
Shane.Pettit <shane.pettit@sita.aero>
2018-03-07 21:02:49 -0500

    Added base edge router

---
+++ edge-router/src/main/java/com/codequest/edge/EdgeRouterApplication.java
@@ -0,0 +1,12 @@
+package com.codequest.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeRouterApplication.class, args);
+	}
+}
